cf03dded2fe46308c04bbd9a20cf386ae09a7523
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-08-03 18:42:35 +0200

    Create clones only if clone bar is full

--- Assets/Scripts/CloneArea.cs
+++ Assets/Scripts/CloneArea.cs
@@ -4,15 +4,28 @@ public class CloneArea : MonoBehaviour
 {
     [SerializeField] ListVector listClonesPosition;
 
+    [SerializeField] GameObject cloneAreaActivated;
+
+    [SerializeField] FloatVariable loadCloneProgression;
+
+    private void Awake()
+    {
+        cloneAreaActivated.SetActive(false);
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (listClonesPosition.CurrentValue.Contains(transform.position))
+        if (
+            listClonesPosition.CurrentValue.Contains(transform.position) ||
+            loadCloneProgression.CurrentValue < 1
+        )
         {
             return;
         }
 
         if (collision.CompareTag("Player"))
         {
+            cloneAreaActivated.SetActive(true);
             listClonesPosition.CurrentValue.Add(transform.position);
         }
     }

b9ad4f86f2a272e25230bcad7ba5703ae751132b
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-08-03 17:58:37 +0200

    Add movements script

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovements : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector3 moveInput = Vector3.zero;
+
+    private Vector2 nextPosition;
+
+    [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        nextPosition = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        rb.linearVelocity = nextPosition;
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        moveInput = (Vector3)ctx.ReadValue<Vector2>();
+    }
+}
